a03136c0ca93cf27a9b0abb7076cf47b09ba790c
Iva <i.totomanova@gmail.com>
2019-01-15 16:21:37 +0100

    Part 2 (unfinished)

--- assignment.py
+++ assignment.py
@@ -5,12 +5,18 @@
 import json
 import csv
 
-open('stations.csv')
-open('precipitation.json')
+#open('stations.csv')
+#open('precipitation.json')
 
 seattle_precipitation = [0]*12 #create an empty list with 12 0s for the 12 months
 seattle_code = "GHCND:US1WAKG0038"
 #rint(station_code)
+
+'''
+with open('stations.csv', encoding='utf-8-sig') as file:
+    reader = csv.reader(file)
+    seattle_code = reader[2][2]
+'''
 
 #for all of the dictionary entries with the station code for seattle, select the value of 'value' for each;
 # also select the value of 'date' for each and sum the values for 'value' for each month
@@ -26,17 +32,6 @@ with open ('precipitation.json', encoding='utf-8-sig') as file:
             seattle_precipitation[month-1] += rainfall
 
 print(seattle_precipitation)
-            #print(measurement["station"])
-        # Print station codes for each measurement
-
-
-
-    #for station in file:
-    #    if "station" == station_code:
-            
-    #        seattle_precipitation.append(["value"])
-            
-#print(seattle_precipitation)
 
 with open('seattle.json', 'w') as file: 
     json.dump(seattle_precipitation, file)
@@ -44,3 +39,11 @@ with open('seattle.json', 'w') as file:
 
 #Part 2: Calculate the sum of the precipitation over the whole year
 #Calculate the relative precipitation per month (percentage compared to the precipitation over the whole year
+year_precipitation = sum(seattle_precipitation[0:])
+print(year_precipitation)
+
+relative_precipitation = [0]*12
+print(relative_precipitation)
+relative_precipitation_jan = (seattle_precipitation[0]/year_precipitation)*100
+print(relative_precipitation_jan)
+#relative_precipitation[month-1] = seattle_precipitation/year_precipitation
